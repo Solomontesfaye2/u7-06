145543d7f4d51bb1c1fd3debe3aacb5ae1d5fcc4
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-29 03:29:57 +0000

    java sc

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let number = 15 
+let number = 15
 let guess = 0
 
-while (guess != number) {
-   guess = prompt('Guess my age')
+while (guess !== number) {
+  guess = prompt('Guess my age')
 }
-while (guess != number) {
-   guess = prompt('Try again')
+while (guess !== number) {
+  guess = prompt('Try again')
 }
