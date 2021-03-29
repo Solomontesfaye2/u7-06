ca7a00d1a4a6121331836480624994304a8b85fa
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-29 04:07:54 +0000

    jv22

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let number = 15
-let guess = 0
+let age = 0
+let day = 0
 
-while (guess !== number) {
-  guess = prompt('Guess my age')
-}
-while (guess !== number) {
-  guess = prompt('Try again')
+document.getElementById('enter').addEventListener('click', enter)
+
+function enter () {
+ day = document.getElementById('input').value
+ if (day === 'Monday' || day === 'Thursday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday')
+ {
+    alert('Time for school!')
+       day = document.getElementById('input2')
+.value
+ }
+ if (age >= 18 && day !== 'Sunday' && day !== 'Saturday')
+   document.getElementById('enter')
+ alert('Time for work')
 }
